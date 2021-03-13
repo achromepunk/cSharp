321093027bcafe393878549fdc7a6db12c73b023
Piquero Cilla <joan.piquero-cilla@capgemini.com>
2021-03-13 20:54:55 +0100

    add listbox example

---
+++ listbox0/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace listbox0
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            int i = 2;
+
+            List<MisNumeros> listaNums = new List<MisNumeros>();
+            listaNums.Add(new MisNumeros() { Uno = i, Dos= i, Tres=i }) ;
+
+            //asi le indicamos quien es el source de listanumeros (ver xaml)
+            //listaNumeros.ItemsSource = listaNums;
+
+        }
+    }
+}
---
+++ listbox0/MisNumeros.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listbox0
+{
+    public class MisNumeros
+    {
+
+        public int uno, dos, tres;
+
+        public int Uno { get { return uno; } set { uno = value; } }
+
+        public int Dos { get { return Dos; } set { Dos = value; } }
+
+        public int Tres { get { return Tres; } set { Tres = value; } }
+    }
+}
